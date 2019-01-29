50b9ce0a9cf22ea44df6cb82ecc1cdfaf9eb04b5
Alexander Pylypenko <Deflamess2@gmail.com>
2019-01-29 20:44:11 +0200

    PSR 2 fix

--- homework/oop2/Accounting/action.php
+++ homework/oop2/Accounting/action.php
@@ -2,18 +2,17 @@
 require_once('TimeAccounting.php');
 require_once('UsernameNotSetException.php');
 
-try
-{
+try {
     $name = trim(stripslashes($_POST['employee_name']));
     $dep = trim(stripslashes($_POST['department']));
 
     $work = new TimeAccounting($name, $dep, 'test.log');
-    if($_POST['work_start']) {
+    if ($_POST['work_start']) {
         $dt = $_POST['work_start'];
         $work->workBegins($dt);
     }
 
-    if($_POST['work_end']) {
+    if ($_POST['work_end']) {
         $dt = $_POST['work_end'];
         $work->workEnds($dt);
     } 
@@ -21,7 +20,7 @@ try
     echo $work->worked();
     $work->writeData();
     
-} catch (UsernameNotSetException $e) { 
-      echo $e->getMessage() . "<br>";
-      echo $e->getCode();
- }
+} catch (UsernameNotSetException $e) {
+        echo $e->getMessage() . "<br>";
+        echo $e->getCode();
+}
