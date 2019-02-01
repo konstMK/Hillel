7df40f188d4f6c23000bf5d110908ed4e521f419
Alexander Pylypenko <Deflamess2@gmail.com>
2019-02-01 23:00:20 +0200

    Sql practise with PDO

--- homework/oop4_SQL/SqlController.php
+++ homework/oop4_SQL/SqlController.php
@@ -4,12 +4,21 @@ class SqlController
 {
     public $label;
     private $sql;
+    private $pdo;
     public $key;
     public $value;
+    public $data;
 
     public function __construct()
     {
-        //$dbh = new PDO("", root, '');
+
+        try {
+            $pdo = new PDO('mysql:host=127.0.0.1; dbname=hillel_hm', 'root', '');
+            $this->pdo = $pdo;
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
     }
 
 
@@ -23,7 +32,7 @@ class SqlController
 
         switch( $this->label ) {
             case "all":
-                return $this->sql = 'SELECT id, first_name, last_name FROM `hillel_hm`.`clients`';
+                return $this->sql = 'SELECT id, first_name, last_name, email, age FROM `hillel_hm`.`clients`';
             case "active":
                 return $this->sql = 'SELECT id, first_name, last_name FROM `hillel_hm`.`clients` where `is_active`= 1';
             case "age":
@@ -47,37 +56,93 @@ class SqlController
 
     }
 
-    public function addClient( string $name, string $last_name, string $email, string $company, int $age )
+    /**
+     * @param string $first_name
+     * @param string $last_name
+     * @param string $email
+     * @param string $company
+     * @param int $is_sctive
+     * @param int $age
+     */
+    public function addClient( string $first_name, string $last_name, string $email, string $company, int $is_sctive, int $age )
     {
-        $this->sql = "INSERT INTO `hillel_hm`.`clients` (
-                                                          `first_name`,
-                                                          `last_name`, 
-                                                          `email`,
-                                                          `company_name`,
-                                                          `is_active`,
-                                                          `age`
+        $stmt = $this->pdo->prepare( "INSERT INTO `hillel_hm`.`clients` (
+                                                                    `first_name`,
+                                                                    `last_name`, 
+                                                                    `email`,
+                                                                    `company_name`,
+                                                                    `is_active`,
+                                                                    `age`
                       ) 
-                      VALUES (':first_name', ':last_name', ':email', ':company', ':is_active', ':age')";
+                      VALUES (:first_name, :last_name, :email, :company, :is_active, :age)" );
+
+
+        $stmt->execute(
+            [
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email,
+                'company' => $company,
+                'is_active' => $is_sctive,
+                'age' => $age,
+            ]
+        );
     }
 
-    protected function updateClient( int $id, $key, $value )
+
+    /**
+     * @param int $id
+     * @param $key
+     * @param $value
+     */
+    protected function updateClient( int $id, $key, $operator, $value )
     {
         if ( $id >= 0 ) {
-            $this->id = $id;
-            $this->key = $key;
-            $this->value = $value;
-            $this->sql = "UPDATE `hillel_hm`.`clients` SET $this->key = $this->value WHERE id = $this->id";
+            $this->queryData($this->sql = "UPDATE `hillel_hm`.`clients` 
+                                           SET $key $operator $value 
+                                           WHERE id = $id"
+            );
         }
     }
 
-    protected function deleteClient( string $key, $value )
+    /**
+     * @param $id
+     * @param $key
+     * @param $value
+     */
+    public function update( $id, $key, $operator, $value )
     {
-        $this->sql = "DELETE FROM `hillel_hm`.`clients` WHERE $key = $value";
+        $this->updateClient($id, $key, $operator, $value);
+    }
+
+    /**
+     * query sql statement
+     */
+    public function queryData($sql)
+    {
+        return $this->pdo->query($sql);
+    }
+
+
+    public function fetchData( $sql )
+    {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+        /*$stmt = $this->pdo->fetch($sql);
+        foreach ( $stmt as $row ) {
+            echo $row['id'] . $row['first_name'] . "\n";
+        }*/
+    }
+
+
+    protected function deleteClient( string $key, $operator, $value )
+    {
+        return "DELETE FROM `hillel_hm`.`clients` WHERE $key $operator $value";
     }
 
     public function delete( string $key, $value )
     {
-        $this->deleteClient( $key, $value );
+        $this->queryData( $this->deleteClient( $key, $operator, $value ) );
     }
 }
 /*
@@ -90,3 +155,10 @@ class SqlController
  */
 
 
+$class = new SqlController();
+//$class->addClient('Женя', 'Иванов','t@r.cor', 'Intel', 1, 33);
+//print_r($class->fetchData($class->showInfo()));
+$class->update(1, 'age', '=', 38);
+print_r($class->fetchData($class->showInfo()));
+//$class->delete('is_active', '>', 6 );
+//var_dump($class);
